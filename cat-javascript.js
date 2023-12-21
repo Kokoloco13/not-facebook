51d7b95197a5d71fdb294040897ac0402364f105
Kokoloco13 <149543178+Kokoloco13@users.noreply.github.com>
2023-12-21 10:02:45 -0700

    Add files via upload

---
+++ cat-javascript.js
@@ -0,0 +1,129 @@
+function followOliver(){
+    var followOliver = document.getElementById("followOliver");
+    if (followOliver.classList.contains("following")) {
+        followOliver.innerHTML = "Follow";
+        followOliver.classList.add("follow-button");
+        followOliver.classList.remove("following");
+    } else {
+        followOliver.innerHTML = "Following";
+        followOliver.classList.add("following");
+        followOliver.classList.remove("follow-button");
+    }
+}
+
+function followBilly(){
+    var followBilly = document.getElementById("followBilly");
+    if (followBilly.classList.contains("following")) {
+        followBilly.innerHTML = "Follow";
+        followBilly.classList.add("follow-button");
+        followBilly.classList.remove("following");
+    } else {
+        followBilly.innerHTML = "Following";
+        followBilly.classList.add("following");
+        followBilly.classList.remove("follow-button");
+    }
+}
+
+function followNibbles(){
+    var followNibbles = document.getElementById("followNibbles");
+    if (followNibbles.classList.contains("following")) {
+        followNibbles.innerHTML = "Follow";
+        followNibbles.classList.add("follow-button");
+        followNibbles.classList.remove("following");
+    } else {
+        followNibbles.innerHTML = "Following";
+        followNibbles.classList.add("following");
+        followNibbles.classList.remove("follow-button");
+    }
+}
+
+function followLucky(){
+    var followLucky = document.getElementById("followLucky");
+    if (followLucky.classList.contains("following")) {
+        followLucky.innerHTML = "Follow";
+        followLucky.classList.add("follow-button");
+        followLucky.classList.remove("following");
+    } else {
+        followLucky.innerHTML = "Following";
+        followLucky.classList.add("following");
+        followLucky.classList.remove("follow-button");
+    }
+}
+
+function followBubbles(){
+    var followBubbles = document.getElementById("followBubbles");
+    if (followBubbles.classList.contains("following")) {
+        followBubbles.innerHTML = "Follow";
+        followBubbles.classList.add("follow-button");
+        followBubbles.classList.remove("following");
+    } else {
+        followBubbles.innerHTML = "Following";
+        followBubbles.classList.add("following");
+        followBubbles.classList.remove("follow-button");
+    }
+}
+
+function followSilky(){
+    var followSilky = document.getElementById("followSilky");
+    if (followSilky.classList.contains("following")) {
+        followSilky.innerHTML = "Follow";
+        followSilky.classList.add("follow-button");
+        followSilky.classList.remove("following");
+    } else {
+        followSilky.innerHTML = "Following";
+        followSilky.classList.add("following");
+        followSilky.classList.remove("follow-button");
+    }
+}
+
+function followBecky(){
+    var followBecky = document.getElementById("followBecky");
+    if (followBecky.classList.contains("following")) {
+        followBecky.innerHTML = "Follow";
+        followBecky.classList.add("follow-button");
+        followBecky.classList.remove("following");
+    } else {
+        followBecky.innerHTML = "Following";
+        followBecky.classList.add("following");
+        followBecky.classList.remove("follow-button");
+    }
+}
+
+function followHoney(){
+    var followHoney = document.getElementById("followHoney");
+    if (followHoney.classList.contains("following")) {
+        followHoney.innerHTML = "Follow";
+        followHoney.classList.add("follow-button");
+        followHoney.classList.remove("following");
+    } else {
+        followHoney.innerHTML = "Following";
+        followHoney.classList.add("following");
+        followHoney.classList.remove("follow-button");
+    }
+}
+
+function followOrci(){
+    var followOrci = document.getElementById("followOrci");
+    if (followOrci.classList.contains("following")) {
+        followOrci.innerHTML = "Follow";
+        followOrci.classList.add("follow-button");
+        followOrci.classList.remove("following");
+    } else {
+        followOrci.innerHTML = "Following";
+        followOrci.classList.add("following");
+        followOrci.classList.remove("follow-button");
+    }
+}
+
+function followCharlie(){
+    var followCharlie = document.getElementById("followCharlie");
+    if (followCharlie.classList.contains("following")) {
+        followCharlie.innerHTML = "Follow";
+        followCharlie.classList.add("follow-button");
+        followCharlie.classList.remove("following");
+    } else {
+        followCharlie.innerHTML = "Following";
+        followCharlie.classList.add("following");
+        followCharlie.classList.remove("follow-button");
+    }
+}
